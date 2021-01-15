b8be86249584fb2d3aa4b1b691b8dcc85f61919c
Aleksander Wyżykowski <wyzykjr@gmail.com>
2021-01-15 18:28:31 +0100

    blabla

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "struktury.h"
 
+
+
 void WypiszPlansze(){
   for(int Pole = 0; Pole < 128; Pole++){
     if(!(Pole % 16)) printf(" %d. ", 8 - ( Pole / 16 ));
@@ -10,12 +12,21 @@ void WypiszPlansze(){
   
   printf("\n     a b c d e f g h\n\nTwoj ruch: \n");
 }
+
+int PrzypiszBierke(int Zrodlowy_Kw){
+    int Bierka;
+    Bierka = Plansza[Zrodlowy_Kw];
+
+    return Bierka;
+}
+
+
 void MozliweRuchy(int Strona){
     int Bierka, Rodzaj, Kierunek, Docelowy_Kw, Zbity_Kw, Zbita_Bierka, Przejscie;
     printf("Mozliwe ruchy to:\n");
     for(int Zrodlowy_Kw = 0; Zrodlowy_Kw < 128; Zrodlowy_Kw++){
         if(!(Zrodlowy_Kw & 0x88)){
-            Bierka = Plansza[Zrodlowy_Kw];
+            Bierka = PrzypiszBierke(Zrodlowy_Kw);
 
             if(Bierka & Strona){
 
@@ -26,7 +37,7 @@ void MozliweRuchy(int Strona){
                 while(ListaRuchow[++Kierunek] != 0){
                     Docelowy_Kw = Zrodlowy_Kw + ListaRuchow[Kierunek];
                     if(!(Docelowy_Kw & 0x88)){
-                        Zbita_Bierka=Plansza[Docelowy_Kw];
+                        Zbita_Bierka = PrzypiszBierke(Docelowy_Kw);
                         if(!(Zbita_Bierka & Strona)){   //Do tego momentu program skanuje kazde pole w poszukiwaniu bierek
                                                         // z danego koloru i wypisuje pierwsze wolne pola na ktore moze sie ruszyc bierka
                             if(Bierka == 9){            //jesli bierka to bialy pionek
@@ -34,19 +45,19 @@ void MozliweRuchy(int Strona){
                                     printf("%s%s  ", notacja[Zrodlowy_Kw], notacja[Docelowy_Kw]);   //napisz ze mozliwy ruch na to pole
                                 }
                                 Docelowy_Kw -= 2;       //zmien docelowe pole na to drugie po skosie
-                                Zbita_Bierka = Plansza[Docelowy_Kw]; //zaktualizuj informacje o bierce na tym polu
+                                Zbita_Bierka = PrzypiszBierke(Docelowy_Kw); //zaktualizuj informacje o bierce na tym polu
                                 if(Zbita_Bierka & 16){              //jesli pole na ktore ma sie ruszyc po skosie jest czarnym pionkiem
                                     printf("%s%s  ", notacja[Zrodlowy_Kw], notacja[Docelowy_Kw]);   //napisz ze mozliwy ruch na to pole
                                 }
                                 Kierunek++;             //Zmien na kolejny ruch
                                 Docelowy_Kw = Zrodlowy_Kw + ListaRuchow[Kierunek];  //Zmien docelowe pole na ktore ma sie ruszyc pionek
-                                Zbita_Bierka = Plansza[Docelowy_Kw];                //Zaktualizuj info o bierce na tym polu
+                                Zbita_Bierka = PrzypiszBierke(Docelowy_Kw);                //Zaktualizuj info o bierce na tym polu
                                 if((!(Zbita_Bierka & 8)) && (!(Zbita_Bierka & 16))){
                                     printf("%s%s  ", notacja[Zrodlowy_Kw], notacja[Docelowy_Kw]);   //napisz ze mozliwy ruch na to pole
                                 
                                     Kierunek++;
                                     Docelowy_Kw = Zrodlowy_Kw + ListaRuchow[Kierunek];
-                                    Zbita_Bierka = Plansza[Docelowy_Kw]; 
+                                    Zbita_Bierka = PrzypiszBierke(Docelowy_Kw); 
                                     if(((Zrodlowy_Kw>=96) && (Zrodlowy_Kw<=104)) && ((!(Zbita_Bierka & Strona)) && (!(Zbita_Bierka & 16)))){
                                         printf("%s%s  ", notacja[Zrodlowy_Kw], notacja[Docelowy_Kw]);
                                     
